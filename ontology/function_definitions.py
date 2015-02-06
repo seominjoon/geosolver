539475de499f88342bb865a7342c3fa1f44156bb
Clint Malcolm <clintm3@cs.washington.edu>
2015-02-06 11:45:30 -0800

    Added many of functions, added basic solver

--- ontology/function_definitions.py
+++ ontology/function_definitions.py
@@ -1,30 +1,22 @@
 import sympy
 from geosolver.ontology.instantiator_definitions import instantiators
 from geosolver.ontology.states import Truth
+import itertools
+from bokeh._glyph_functions import line
 
 __author__ = 'minjoon'
 
 def and_(truth0, truth1):
     """
-    TO BEIMPLEMENTED
-    :param truth0:
-    :param truth1:
-    :return:
+    TODO: include t.sigma
     """
-    isinstance(truth0, Truth)
-    isinstance(truth1, Truth)
-    truth0.expression + truth1.expression
+    return max([truth0, truth1], key=lambda t: t.expression.sort_key())
 
 def or_(truth0, truth1):
-    """
-    TO BE IMPLEMENTED
-    :param truth0:
-    :param truth1:
-    :return:
-    """
-    truth0.expression * truth1.expression
-    pass
+    return min([truth0, truth1], key=lambda t: t.expression.sort_key())
 
+def not_(truth):
+    return Truth(-truth.expression, truth.sigma)
 
 def add(number0, number1):
     return number0 + number1
@@ -47,20 +39,20 @@ def pow(number0, number1):
 
 
 def equal(number0, number1):
-    expr = sympy.abs(number0 - number1)
-    sig = sympy.abs(number0 + number1) / 2
+    expr = abs(number0 - number1)
+    sig = abs(number0 + number1) / 2
     return Truth(expr, sig)
 
 
 def greater(number0, number1):
     expr = number1 - number0
-    sig = sympy.abs(number0 + number1) / 2
+    sig = abs(number0 + number1) / 2
     return Truth(expr, sig)
 
 
 def less(number0, number1):
     expr = number0 - number1
-    sig = sympy.abs(number0 + number1) / 2
+    sig = abs(number0 + number1) / 2
     return Truth(expr, sig)
 
 
@@ -72,7 +64,7 @@ def angleOf_angle(angle):
     a = lengthOf(instantiators['line'](angle.b, angle.c))
     b = lengthOf(instantiators['line'](angle.c, angle.a))
     c = lengthOf(instantiators['line'](angle.a, angle.b))
-    return sympy.sqrt((a**2 + b**2 - c**2) / (2*a*b))
+    return sympy.acos(sympy.sqrt((a**2 + b**2 - c**2) / (2*a*b)))
 
 
 def angleOf_arc(arc):
@@ -87,14 +79,16 @@ def radiusOf(circle):
 def diameterOf(circle):
     return 2 * circle.radius
 
+def sortPointsClockwise(poly):
+    center = tuple(sum(x) for x in zip(*poly))
+    centerX, centerY = (center[0]/len(poly), center[1]/len(poly))
+    angle = lambda (x1,y1): sympy.atan2(y1 - centerY, x1 - centerX)
+    angles = map(lambda p: (p,angle(p)),poly)
+    return map(lambda (p,_): p, sorted(angles, key=lambda (_,ang): ang.sort_key()))
 
-def areaOf_polygon(polygon):
-    """
-    TO BE IMPLEMENTED
-    :param polygon:
-    :return:
-    """
-    pass
+def areaOf_polygon(poly):
+    poly = sortPointsClockwise(poly)
+    return 0.5 * abs(sum(x0 * y1 - x1 * y0 for ((x0, y0), (x1, y1)) in zip(poly, poly[1:] + poly[:1])))
 
 
 def areaOf_circle(circle):
@@ -110,28 +104,54 @@ def circumferenceOf(circle):
     return 2 * sympy.pi * circle.radius
 
 
-def intersects(entity0, entity1):
-    """
-    TO BE IMPLEMENTED (LAST PRIORRITY)
-    :param entity0:
-    :param entity1:
-    :return:
-    """
-    if isinstance(entity0, instantiators['line']):
-        pass
+def intercept(line0,line1):
+    s0 = slope(*line0)
+    s1 = slope(*line1)
+    xint = (s0*line0[0][0] - s1*line1[0][0] - line0[0][1] + line1[0][1]) / (s0 - s1)
+    if xint == sympy.S("zoo") or xint == sympy.S("oo"):
+        return False
+    yint = (line0[0][0] - xint) * s0 + line0[0][1]
+    return instantiators['point'](xint,yint)
 
+def perpindicularIntercept(line, point):
+    m = slope(*line)
+    ox,oy = point
+    (x1,y1),_ = line
+    x = (m**2 * x1 + m*oy - m * y1 + ox)/(m**2 + 1)
+    y = (-1/m) * (x - ox) + oy
+    perpInt = instantiators['point'](x,y)
+    return instantiators['line'](entity1.center, perpInt)
+
+def intersects(entity0, entity1):
+    if isinstance(entity0, instantiators['line']) and isinstance(entity1, instantiators['line']):
+        p = intercept(entity0, entity1)
+        if not p:
+            return not_(equal(s0,s1))
+        return and_(not_(equal(s0, s1)),
+                    and_(isOn(p, entity0),
+                         isOn(p, entity1)))
+    elif isinstance(entity0, instantiators['circle']) and isinstance(entity1, instantiators['circle']):
+        dist = lengthOf(instantiators['line'](entity0.center, entity1.center))
+        return less(dist, entity0.radius + entity1.radius)
+    elif isinstance(entity0, ['circle']):
+        return intersects(entity1, entity0)
+    perpLine = perpindicularIntercept(entity0, entity1.center)
+    return less(lengthOf(perpLine), entity1.radius)
+                         
+
+def slope((x1,y1),(x2,y2)):
+    return (x1 - x2)/(y1 - y2)
 
 def isOn(point, entity):
-    """
-    TO BE IMPLEMENTED
-    :param point:
-    :param entity:
-    :return:
-    """
     if isinstance(entity, instantiators['line']):
-        pass
+        ang = angleOf_angle(instantiators['angle'](entity[0],point,entity[1]))
+        betweenX = or_(and_(less(entity[0].x, point.x), less(point.x, entity[1].x)),
+                       and_(less(entity[1].x, point.x), less(point.x, entity[0].x)))
+        betweenY = or_(and_(less(entity[0].y, point.y), less(point.y, entity[1].y)),
+                       and_(less(entity[1].y, point.y), less(point.y, entity[0].y)))
+        return and_(and_(equal(ang, 0), betweenX), betweenY)
     elif isinstance(entity, instantiators['circle']):
-        pass
+        return equal(lengthOf(instantiators['line'](entity.center, point)), entity.radius)
 
 
 def isMidpointOf(point, line):
@@ -139,5 +159,105 @@ def isMidpointOf(point, line):
     b = lengthOf(instantiators['line'](point, line.b))
     return equal(a, b)
 
+def isParallelWith(line0,line1):
+    return equals(slope(*line0), slope(*line1))
 
+def isPerpendicularTo(line0, line1):
+    s0 = slope(*line0)
+    perpSlope = 1 / (-s0)
+    return equals(perpSlope, slope(*line1))
+
+def bisects_line(line0, line1):
+    int = intercept(line0, line1)
+    if not int:
+        #TODO: something real
+        return Truth(-1000,0)
+    subline0 = instantiators['line'](line0[0],int)
+    subline1 = instantiators['line'](int,line0[1])
+    return equal(lengthOf(subline0), lengthOf(subline1))
+def bisects_angle(line, angle):
+    pass
+def isRightAngle(angle):
+    return equal(angleOf_angle(angle), sympy.pi / 2)
+def nInARow(l,n):
+    dl = list(itertools.chain(quad,quad))
+    return [dl[i:i+n] for i in xrange(len(l))]
+def isHypotenuseOf(line, triangle):
+    pass
+def isMedianOf(line, triangle):
+    pass
+def isAltitudeOf(line, triangle):
+    pass
+def isIsosceles(tri):
+    lines = itertools.combinations(tri, 2)
+    line_lengths = map(lambda l: lengthOf(instantiators['line'](l)), lines)
+    line_length_pairs = itertools.combinations(line_lengths, 2)
+    return reduce(lambda prior, n: n if not prior else or_(prior, n),
+                  map(lambda lengths: equal(*lengths),line_length_pairs),
+                  False)
+    
+def isRightTriangle(tri):
+    angles = map(lambda order: isRightAngle(instantiators['angle'](*order)), nInARow(tri, 3))
+    return reduce(lambda prev, n: n if not prev else or_(prev, n),
+                  angles,
+                  False)
+    
+def isEquilateral(tri):
+    is60Deg = lambda ordering: equal(angleOf_angle(instantiators['angle'](ordering)), sympy.pi / 3)
+    return reduce(lambda prev, n: n if not prev else and_(prev, n),
+                  map(is60Deg, nInARow(tri, 3)),
+                  False)
+def isParallelogram(quad):
+    quad = sortPointsClockwise(quad)
+    lines = zip(quad, quad[1:] + quad[:1])
+    lengths = map(lambda line: lengthOf(instantiators['line'](line)), lines)
+    return and_(equal(lengths[0], lengths[2]),
+                equal(lengths[1], lengths[3]))
+def isRhombus(quad):
+    quad = sortPointsClockwise(quad)
+    lines = zip(quad, quad[1:] + quad[:1])
+    lengths = map(lambda line: lengthOf(instantiators['line'](line)), lines)
+    return and_(equal(lengths[0], lengths[1]),
+                and_(equal(lengths[0], lengths[2]),
+                     equal(lengths[1], lengths[3])))
+def isRectangle(quad):
+    is90Deg = lambda ordering: isRightAngle(instantiators['angle'](ordering))
+    return reduce(lambda prev, n: n if not prev else and_(prev, n),
+                  map(is60Deg, nInARow(quad, 3)),
+                  False)
+def isSquare(quad):
+    quad = sortPointsClockwise(quad)
+    return and_(isRectangle(quad), isRhombus(quad))
+def isRadiusOf(line, circle):
+    distFromCenterA = lengthOf(instantiators['line'](line[0], circle.center))
+    distFromCenterB = lengthOf(instantiators['line'](line[1], circle.center))
+    return and_(equal(lengthOf(line), circle.radius),
+                or_(equal(distFromCenterA, 0),
+                    equal(distFromCenterB, 0)))
+def isDiameterOf(line, circle):
+    return and_(isMidpointOf(circle.center, line),
+                equal(diameterOf(circle), lengthOf(line)))
+def isChordOf(line, circle):
+    return and_(isOn(line[0], circle),
+                isOn(line[1], circle))
+                           
+def isTangentOf(line, circle):
+    line0 = instantiators['line'](line[0], circle.center)
+    line1 = instantiators['line'](line[1], circle.center)
+    return and_(equal(lengthOf(perpindicularIntercept(line, circle.center))),
+                and_(greater(lengthOf(line0), circle.radius),
+                     greater(lengthOf(line1), circle.radius)))
+                       
+def isSecantOf(line, circle):
+    line0 = instantiators['line'](line[0], circle.center)
+    line1 = instantiators['line'](line[1], circle.center)
+    return and_(not_(equal(lengthOf(perpindicularIntercept(line, circle.center)),
+                           circle.radius)),
+                and_(intersects(line, circle),
+                     and_(greater(lengthOf(line0), circle.radius),
+                          greater(lengthOf(line1), circle.radius))))
+                
 # And so on...
+p = instantiators['point'](sympy.S('x - 1'),sympy.S('y - 2'))
+l = instantiators['line'](instantiators['point'](sympy.S('x'),sympy.S('y')), instantiators['point'](sympy.S('x+3'),sympy.S('y+6')))
+print((isOn(p,l).expression).evalf())
--- solver/algebraic_solver.py
+++ solver/algebraic_solver.py
@@ -1,5 +1,8 @@
+from _functools import partial
 __author__ = 'minjoon'
 
+from scipy.optimize import minimize
+import sympy
 
 def algebraic_solver(equations, initial_values):
     """
@@ -10,4 +13,17 @@ def algebraic_solver(equations, initial_values):
     :param equations:
     :return dict:
     """
-    pass
+    indexToVariable = {i: var for (i,var) in enumerate(initial_values.keys())}
+    evalExp = partial(evalFunctions, equations, indexToVariable)
+    result = minimize(evalExp, initial_values.values(), method='SLSQP', options={'ftol': 10**-9, 'maxiter': 10000})
+    
+    return {indexToVariable[i]:val for (i,val) in enumerate(result.x)}
+    
+def evalFunctions(equations, indexToVariable, x):
+    varToVal = [(indexToVariable[i], val) for (i,val) in enumerate(x)]
+    return sum(eq.subs(varToVal).evalf() ** 2 for eq in equations)
+
+x = sympy.S(5)
+y = sympy.S(1)/sympy.S(0)
+print(max([x,y], key=lambda t:t.sort_key()))
+print(algebraic_solver([sympy.S("x**2 - y**2"),sympy.S("x*y - x")], {sympy.S("x"):100, sympy.S("y"):5000}))
